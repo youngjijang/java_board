46c6385b955f03697ebc28cfc5e99c187c3d2883
youngjijang <youngji.jang804@gmail.com>
2024-06-04 12:59:42 +0900

    first commit

---
+++ src/main/java/com/example/java_board/JavaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_board/JavaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
